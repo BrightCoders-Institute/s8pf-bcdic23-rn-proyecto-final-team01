bee0926cd03b025cb984ddeab0c5a7e325c0c25b
ernesto2001-code <74994062+ernesto2001-code@users.noreply.github.com>
2024-03-11 18:44:19 -0600

    Update MapScreen.tsx
    refactoring code

--- Roamify/src/screens/MapScreen.tsx
+++ Roamify/src/screens/MapScreen.tsx
@@ -8,28 +8,16 @@ import GoogleMapComponent from '../components/googlemaps/GoogleMapComponent';
 
 const MapScreen = () => {
   const navigation = useNavigation();
-
   return (
     <View style={globalStyles.screen}>
       <Header />
       <View style={{width: '100%', height: '78%'}}>
       <GoogleMapComponent >
-
       </GoogleMapComponent>
       </View>
       <NavBar />
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingHorizontal: 15,
-  },
-});
 
 export default MapScreen;
