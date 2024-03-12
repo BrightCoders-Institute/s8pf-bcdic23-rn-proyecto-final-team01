ef27df83ed83dd0c2e09c09d47b7f2195ef878e4
Vanessa Covarrubias <vanessa.covarrubias.nava@gmail.com>
2024-03-12 17:27:11 -0600

    Adding FlatListComponent and Data

---
+++ Roamify/src/API/EventsData.js
@@ -0,0 +1,23 @@
+const DATA = [
+    {
+      id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+      title: 'La petatera',
+      description: 'La petatera es',
+      sample: require('../assets/sample-image.jpg')
+    },
+    {
+      id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+      title: 'Second Item',
+      description: 'La petatera es',
+      sample: require('../assets/sample-image.jpg')
+  
+    },
+    {
+      id: '58694a0f-3da1-471f-bd96-145571e29d72',
+      title: 'Third Item',
+      description: 'La petatera es',
+      sample: require('../assets/sample-image.jpg')  
+    },
+  ];
+
+  export default DATA;
