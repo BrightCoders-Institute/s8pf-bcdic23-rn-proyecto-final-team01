4921bf7ce68e340e565b1d3d3f01f98065668a36
Erick Cuevas <ecuevasceballos@gmail.com>
2024-03-14 17:23:29 -0600

    Form add event update
    - Error handling
    - Image upload to firebase storage
    - Add event to firestore

--- Roamify/src/components/FormAddEvent.tsx
+++ Roamify/src/components/FormAddEvent.tsx
@@ -8,20 +8,22 @@ import {useForm} from 'react-hook-form';
 import {yupResolver} from '@hookform/resolvers/yup';
 import * as yup from 'yup';
 import CheckBoxComponent from './CheckBoxComponent';
-import InputFileComponent from './InputFileComponent';
 import firestore from '@react-native-firebase/firestore';
 import InputMapComponent from './InputMapComponent';
+import ImagePickerComponent from './ImagePickerComponent';
 
 const schemaAddEvent = yup.object().shape({
   nameEvent: yup.string().required('Nombre del evento es requerido'),
-  file: yup.array().required('Archivo no válido'),
+  image: yup.string(),
   descriptionEvent: yup.string().required('La descripción es requerida'),
   date: yup.string(),
   map: yup.object().required('Selecciona una ubicación válida'),
-  /* capacity: yup.boolean(), */
+  limitedCapacity: yup.boolean(),
 });
 
-const FormAddEvent = ({location, setLocation}) => {
+/* @ts-ignore */
+const FormAddEvent = ({location, setLocation, setIsLoading}) => {
+  /* Date set */
   const currentDate = new Date();
 
   const formattedDate = `${currentDate.getFullYear()}-${(
@@ -36,6 +38,18 @@ const FormAddEvent = ({location, setLocation}) => {
     setSelectedDate(dateString);
   };
 
+  /* Limited Capacity check */
+  const [isChecked, setIsChecked] = useState(false);
+
+  /* set image link */
+  const defaultEvent =
+    'https://firebasestorage.googleapis.com/v0/b/roamify-bb95e.appspot.com/o/images%2FdefaultEventImage.jpg?alt=media&token=fa29ddfb-e597-4b1c-9757-7e0b7f55ea5e';
+
+  const [link, setLink] = useState<string>(defaultEvent);
+  const handleSetLink = (downloadLink: string) => {
+    setLink(downloadLink);
+  };
+
   const {
     control,
     handleSubmit,
@@ -47,26 +61,36 @@ const FormAddEvent = ({location, setLocation}) => {
   });
 
   /* @ts-ignore */
-  const onSubmit = () => {
+  const onSubmit = async () => {
+    setIsLoading(true);
     const values = getValues();
-    const {nameEvent, descriptionEvent, file, date, map} = values;
+    const {nameEvent, descriptionEvent, image, date, map, limitedCapacity} =
+      values;
     try {
       firestore()
         .collection('events')
         .add({
           nameEvent,
           descriptionEvent,
-          file,
+          image: link,
           date: selectedDate,
           map,
+          limitedCapacity: isChecked,
         })
         .then(() => {
           console.log('Event added!');
-        });
+        })
+        .catch(error => console.log('Error adding event: ', error));
     } catch (error: any) {
       console.error('Error: ', error);
+    } finally {
+      setTimeout(() => {
+        setIsLoading(false);
+      }, 1000);
     }
   };
+
+  console.log('link value in form: ' + link);
 
   return (
     <View style={styles.container}>
@@ -79,16 +103,8 @@ const FormAddEvent = ({location, setLocation}) => {
         errors={errors}
         name="nameEvent"
       />
-      <TextComponent text="Portada" />
-      <InputFileComponent
-        placeholder="Añade un foto"
-        style={globalStyles.inputPrimary}
-        control={control}
-        setValue={setValue}
-        errors={errors}
-        name="photoEvent"
-        icon="camera"
-      />
+      <TextComponent text="Selecciona una foto de portada" />
+      <ImagePickerComponent errors={errors} setDownloadLink={handleSetLink} />
       <TextComponent text="Selecciona una fecha" />
       <CalendarComponent onDateSelect={handleDateSelect} />
       <TextComponent text="Dirección del evento" />
@@ -111,8 +127,8 @@ const FormAddEvent = ({location, setLocation}) => {
         errors={errors}
         name="descriptionEvent"
       />
-      {/* <TextComponent text="Capacidad limitada" />
-      <CheckBoxComponent /> */}
+      <TextComponent text="Capacidad limitada" />
+      <CheckBoxComponent onPress={() => setIsChecked(!isChecked)} />
       <TouchableOpacity
         style={globalStyles.buttonPrimary}
         onPress={handleSubmit(onSubmit)}>
