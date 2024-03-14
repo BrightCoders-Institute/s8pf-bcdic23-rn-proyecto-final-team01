63ebc0fd38e5fc7a901a6250d3316fa29e90e16e
Erick Cuevas <ecuevasceballos@gmail.com>
2024-03-14 17:22:40 -0600

    Limited capacity event value

--- Roamify/src/components/CheckBoxComponent.tsx
+++ Roamify/src/components/CheckBoxComponent.tsx
@@ -1,25 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import BouncyCheckbox from 'react-native-bouncy-checkbox';
 
 interface Props {
-  text?: string;
+  onPress?: () => void;
 }
 
 const CheckBoxComponent = (props: Props) => {
-  const {text} = props;
+  const {onPress} = props;
 
   return (
     <BouncyCheckbox
       size={25}
       fillColor="#47C6E6"
       unfillColor="#fff"
-      text={text}
       textStyle={{textDecorationLine: 'none', color: '#B6B7BA'}}
       iconStyle={{borderRadius: 0, marginVertical: 10}}
       innerIconStyle={{borderRadius: 5}}
-      onPress={(isChecked: boolean) => {
-        console.log(isChecked);
-      }}
+      onPress={onPress}
     />
   );
 };
