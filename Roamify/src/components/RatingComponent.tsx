518c75cd4e58c3f1e9aa6709ef2518fa3a335206
Erick Cuevas <ecuevasceballos@gmail.com>
2024-04-03 08:01:00 -0600

    Rating functionality

--- Roamify/src/components/RatingComponent.tsx
+++ Roamify/src/components/RatingComponent.tsx
@@ -1,21 +1,71 @@
+import React, {useState} from 'react';
 import {StyleSheet, View} from 'react-native';
-import React from 'react';
 import Icon from 'react-native-vector-icons/Ionicons';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+import TextComponent from './TextComponent';
 
-const RatingComponent = () => {
+/* @ts-ignore */
+const RatingComponent = ({setRating, disabled, obtainedValue, error}) => {
+  const [ratingValue, setRatingValue] = useState(obtainedValue);
+
+  const handleRating = (value: number) => {
+    setRatingValue(value);
+    setRating(value);
+  };
+
+  const rating = [
+    {
+      icon: ratingValue >= 1 ? 'star' : 'star-outline',
+      value: 1,
+    },
+    {
+      icon: ratingValue >= 2 ? 'star' : 'star-outline',
+      value: 2,
+    },
+    {
+      icon: ratingValue >= 3 ? 'star' : 'star-outline',
+      value: 3,
+    },
+    {
+      icon: ratingValue >= 4 ? 'star' : 'star-outline',
+      value: 4,
+    },
+    {
+      icon: ratingValue === 5 ? 'star' : 'star-outline',
+      value: 5,
+    },
+  ];
+
   return (
     <View style={styles.container}>
-      <Icon name="star" color="#A8BD29" size={22} />
-      <Icon name="star" color="#A8BD29" size={22} />
-      <Icon name="star" color="#A8BD29" size={22} />
-      <Icon name="star" color="#A8BD29" size={22} />
-      <Icon name="star-outline" color="#A8BD29" size={22} />
+      <View style={styles.starContainer}>
+        {rating.map(item => (
+          <TouchableOpacity
+            key={item.value}
+            onPress={() => handleRating(item.value)}
+            disabled={disabled}>
+            <Icon name={item.icon} size={22} color="#A8BD29" />
+          </TouchableOpacity>
+        ))}
+      </View>
+      {error && (
+        <TextComponent
+          text="La calificación no puede estar vacía"
+          color="red"
+          size={12}
+        />
+      )}
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: 15,
+  },
+  starContainer: {
     display: 'flex',
     flexDirection: 'row',
     gap: 5,
