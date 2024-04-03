da0a9a51a54b67bf84f7e1d29bfacd3bdbbf1db7
Erick Cuevas <ecuevasceballos@gmail.com>
2024-04-03 11:04:51 -0600

    Maps component

--- Roamify/src/screens/EventDetailsScreen.tsx
+++ Roamify/src/screens/EventDetailsScreen.tsx
@@ -1,13 +1,14 @@
 import React, {useEffect, useState} from 'react';
-import {View, Image, StyleSheet} from 'react-native';
+import {View, Image, StyleSheet, ScrollView} from 'react-native';
 import TextComponent from '../components/TextComponent';
 import FabComponent from '../components/FabComponent';
 import InfoItem from '../components/InfoItem';
-import {ScrollView} from 'react-native-gesture-handler';
 import CommentComponent from '../components/CommentComponent';
 import firestore from '@react-native-firebase/firestore';
 import ReviewComponent from '../components/ReviewComponent';
 import {useAuth} from '../contexts/AuthContext';
+import MapsEventDetailsComponent from '../components/googlemaps/MapsEventDetailsComponent';
+import axios from 'axios';
 
 /* @ts-ignore */
 const EventDetailsScreen = ({navigation, route}) => {
@@ -66,12 +67,14 @@ const EventDetailsScreen = ({navigation, route}) => {
         {data.price && <InfoItem icon="cash" eventData={data.price} />}
         <TextComponent text={data.description} />
       </View>
-      <View style={styles.detailsContainer}>
-        <Image
-          source={require('../assets/map.jpg')}
-          style={styles.image}
-          resizeMode="cover"
+      <View style={styles.mapContainer}>
+        <MapsEventDetailsComponent
+          latitude={data.map.latitude}
+          longitude={data.map.longitude}
+          title={data.name}
         />
+      </View>
+      <View style={styles.detailsContainer}>
         <TextComponent text="Reseñas" font="bold" size={26} />
         <CommentComponent data={reviews} />
         {!hasCommented && <ReviewComponent locationId={id} />}
@@ -92,6 +95,28 @@ const styles = StyleSheet.create({
     padding: 20,
     flexDirection: 'column',
     gap: 10,
+  },
+  mapContainer: {
+    padding: 80,
+    borderRadius: 15,
+    overflow: 'hidden',
+    margin: 20,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  review: {
+    padding: 20,
+  },
+  locationContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 15,
   },
 });
 
